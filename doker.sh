210e2f83796f17603326b2fb2983e220960ef7b2
serverir <homelandmailbox@gmail.com>
2024-12-16 13:42:42 +0330

    Create doker.sh

---
+++ doker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# docker installation
+apt update
+apt remove -y docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+apt install ca-certificates curl gnupg -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+# shellcheck source=/dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+systemctl enable docker
